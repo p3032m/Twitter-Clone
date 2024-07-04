ded24cdf01ee5c5afdab1841b481c68204879ef8
p3032m <prachisawarn453@gmail.com>
2024-07-04 09:51:46 -0700

    first

--- backend/src/configs/env.config.ts
+++ backend/src/configs/env.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('CONFIGS', () => ({
   DATABASE_URL:
-    'mongodb://localhost:27017/',
+    'mongodb+srv://admin:o1V2W5u9B1cmd2LZ@twitter-clon.mmnijt2.mongodb.net/?retryWrites=true&w=majority&appName=twitter-clon',
   PORT: 3001,
   JWT_SECRET: '2EhG8u5VqW54W4Ux9A3f7RbE6S2e0BdK',
 }));
